ea9d5ceab1657f769b9ad09d96c11b7a36a6fb96
ezizull <mfiqor@gmail.com>
2023-04-12 02:20:29 +0700

    add: crud-asset v3

--- application/usecases/assets/mappers.go
+++ application/usecases/assets/mappers.go
@@ -4,15 +4,40 @@ import domainAsset "tennet/gethired/domain/assets"
 
 func (n *NewAsset) toDomainMapper() *domainAsset.Asset {
 	return &domainAsset.Asset{
-		Name: n.Name,
+		WalletID: n.WalletID,
+		Name:     n.Name,
+		Symbol:   n.Symbol,
+		Network:  n.Network,
+		Address:  n.Address,
+		Balance:  n.Balance,
 	}
 }
 
 func (n *UpdateAsset) toDomainMapper() domainAsset.Asset {
 	domainAsset := domainAsset.Asset{}
 
+	if n.WalletID != nil {
+		domainAsset.WalletID = *n.WalletID
+	}
+
 	if n.Name != nil {
 		domainAsset.Name = *n.Name
+	}
+
+	if n.Symbol != nil {
+		domainAsset.Symbol = *n.Symbol
+	}
+
+	if n.Network != nil {
+		domainAsset.Network = *n.Network
+	}
+
+	if n.Address != nil {
+		domainAsset.Address = *n.Address
+	}
+
+	if n.Balance != nil {
+		domainAsset.Balance = *n.Balance
 	}
 
 	return domainAsset
